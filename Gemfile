b02dc0f662e43630abc98656d0515774f1c0a224
Anastasiia Duchenko <nduchenko2001@gmail.com>
2022-02-16 19:23:40 +0200

    make it better

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "down", "~> 5.2"
 
+gem "aws-sdk-s3", require: false
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/javascript/controllers/microlink_controller.js
+++ app/javascript/controllers/microlink_controller.js
@@ -2,19 +2,41 @@ import { Controller } from "stimulus";
 import mql from "@microlink/mql";
 
 export default class extends Controller {
-  static targets = ["input", "imageInput", "output", "button"];
+  static targets = ["input", "imageInput", "output", "button", "message"];
 
   connect() {
     this.previewImage = this.outputTarget.querySelector("img");
   }
 
   async handleChange() {
-    const { status, data } = await mql(this.inputTarget.value);
-    if (status == "success") {
-      this.setFormData(data);
-      this.renderPreview(data);
-      this.buttonTarget.disabled = false;
+    this.messageTarget.innerText = null;
+    this.clearFormData();
+    this.clearPreview();
+    if (this.inputTarget.value != "") {
+      try {
+        this.messageTarget.innerText = "Loading preview...";
+        const { status, data } = await mql(this.inputTarget.value);
+        if (status == "success") {
+          this.setFormData(data);
+          this.renderPreview(data);
+          this.messageTarget.innerText = null;
+          this.buttonTarget.disabled = false;
+        } else {
+          this.messageTarget.innerText =
+            "There was an error fetching the link preview.";
+        }
+      } catch (e) {
+        this.messageTarget.innerText =
+          "There was an error fetching the link preview.";
+      }
     }
+  }
+  clearFormData() {
+    this.imageInputTarget.value = null;
+  }
+  clearPreview() {
+    this.previewImage.setAttribute("src", "");
+    this.outputTarget.style.display = "none";
   }
   renderPreview(data) {
     data?.image?.url
